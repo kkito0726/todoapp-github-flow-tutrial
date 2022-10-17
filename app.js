c041756105864256b160e3c2f1f471b9f5b1562a
kkito0726 <k.kito0726@gmail.com>
2022-10-17 18:29:00 +0900

    完了未完了の実装が完了しました

--- app.js
+++ app.js
@@ -5,4 +5,12 @@ $("#addTodo").click(function () {
 
     // テキストボックスの中身を空にする。
     $("input").val("");
-})
+});
+
+$(document).on("change", "input[type=checkbox]", function () {
+    if ($(this).is(":checked")) {
+        $(this).parent().css("text-decoration", "line-through");
+    } else {
+        $(this).parent().css("text-decoration", "none");
+    }
+});
